5bd3b2f943be08eb7df1b9641efc9519a6352798
Rich Pecson <richard_pecson@dlsu.edu.ph>
2023-11-19 04:14:11 +0800

    added link

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,7 @@
 
 import React, { useState } from 'react';
 import Navbar from "../components/Navbar";
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const Signup = () => {
 
@@ -46,7 +47,9 @@ const Signup = () => {
                           <label for="terms" class="font-light text-white">Na-verify ko na tama ang impormasyon na inilagay ko.</label>
                         </div>
                     </div>
-                    <button type="submit" class="w-full text-white bg-highlight hover:bg-higlight focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Gumawa ng account</button>
+                    <Link to="/">
+                    <button type="submit" class="w-full text-white bg-highlight hover:bg-higlight focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm mt-4 px-5 py-2.5 text-center">Gumawa ng account</button>
+                    </Link>
                     <p class="text-sm font-light text-white">
                         May account ka na? <a href="#" class="font-medium text-primary-600 hover:underline">Login dito</a>
                     </p>
