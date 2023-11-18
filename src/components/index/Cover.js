ad31423a88589360f1122e14d2fb7aa67b62425b
Rich Pecson <richard_pecson@dlsu.edu.ph>
2023-11-19 03:12:16 +0800

    added navbar and updated index page

--- src/components/index/Cover.js
+++ src/components/index/Cover.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import mainImage from '../../assets/cover-image.jpg';
 class Cover extends Component {
   render() {
     return (
-      <section className="bg-center bg-no-repeat bg-blend-multiply bg-background">
-      <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-48 mt-16">
+      <section className="bg-center bg-no-repeat bg-blend-multiply bg-background h-screen">
+      <div className="px-4 mx-auto max-w-screen-xl text-center py-8 lg:py-48">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">
-          Name of our application
+          Ano ang Kitamo?
         </h1>
+        <img src={mainImage} className="mb-4"/>
         <p className="mb-8 text-lg font-normal text-white lg:text-xl sm:px-16 lg:px-48">
-          This is the description of the application
+          Ang Kitamo ay isang AI chatbot na makakatulong sa iyo sa iyong mga problema.
         </p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <Link
             to="/signup"
             className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-gray-100 hover:bg-highlight hover:text-white focus:ring-4 focus:ring-gray-400"
           >
-            Get started
+            Simula na tayo!
             <svg
               className="w-3.5 h-3.5 ml-2"
               aria-hidden="true"
@@ -34,12 +35,6 @@ class Cover extends Component {
               />
             </svg>
           </Link>
-          <a
-            href="#"
-            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white focus:ring-4 focus:ring-gray-400"
-          >
-            Learn more
-          </a>
         </div>
       </div>
     </section>
