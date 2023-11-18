a7d1cfaf144840f2b9c2fef558131e904ae7ec84
Rich Pecson <richard_pecson@dlsu.edu.ph>
2023-11-19 01:19:36 +0800

    add: index cover

---
+++ src/components/index/Cover.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Cover extends Component {
+  render() {
+    return (
+      <section className="bg-center bg-no-repeat bg-blend-multiply bg-background">
+      <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-48 mt-16">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">
+          Name of our application
+        </h1>
+        <p className="mb-8 text-lg font-normal text-white lg:text-xl sm:px-16 lg:px-48">
+          This is the description of the application
+        </p>
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+          <Link
+            to="/student/signup"
+            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-gray-100 hover:bg-highlight hover:text-white focus:ring-4 focus:ring-gray-400"
+          >
+            Get started
+            <svg
+              className="w-3.5 h-3.5 ml-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Link>
+          <a
+            href="#"
+            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white focus:ring-4 focus:ring-gray-400"
+          >
+            Learn more
+          </a>
+        </div>
+      </div>
+    </section>
+    )
+    
+  }
+}
+
+export default Cover;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        logo: "#236B51",
+        highlight: "#458579",
+        highlight_hover: "#245B72",
+        background: "#51A98F",
+        background_pastel: "#E8F4F0",
+        required_red: "#FF0000",
+      }
+    },
   },
   plugins: [],
 }
