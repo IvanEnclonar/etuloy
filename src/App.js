57012906baf0945805a2a08bc9968280eec6229c
IvanEnclonar <paulivanenclonar@gmail.com>
2023-11-18 17:07:27 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1 className="text-red-900">Learn React</h1>
         </a>
       </header>
     </div>
