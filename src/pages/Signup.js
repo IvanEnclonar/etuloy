e00c1a80d65a57763dc6cf859c0fc38ca3fe529b
Rich Pecson <richard_pecson@dlsu.edu.ph>
2023-11-19 03:13:36 +0800

    fixed UI of signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,24 +1,26 @@
 
 import React, { useState } from 'react';
+import Navbar from "../components/Navbar";
 
 const Signup = () => {
 
   return (
     <div>
-      <section class="bg-highlight_hover">
-          <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-          <a href="#" class="flex items-center mb-6 text-3xl text-white uppercase font-bold">
+      <Navbar />
+      <section className="bg-center bg-no-repeat bg-blend-multiply bg-background h-screen py-8">
+          <div class="flex flex-col items-center justify-center px-6 mx-auto lg:py-0">
+          <a href="#" class="flex items-center mb-6 text-3xl text-white uppercase font-bold text-center">
               Kami bahala sa'yo!   
           </a>
-          <div class="w-full bg-highlight rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+          <div class="w-full bg-highlight_hover rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 class="text-xl font-semibold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Create and account
+                    Create an account
                 </h1>
                 <form class="space-y-4 md:space-y-6" action="#">
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required=""/>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="user@email.com" required=""/>
                     </div>
                     <div>
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
@@ -36,7 +38,7 @@ const Signup = () => {
                           <label for="terms" class="font-light text-white">I accept the <a class="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</a></label>
                         </div>
                     </div>
-                    <button type="submit" class="w-full text-white bg-highlight_hover hover:bg-higlight_hover focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
+                    <button type="submit" class="w-full text-white bg-highlight hover:bg-higlight focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
                     <p class="text-sm font-light text-white">
                         Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline">Login here</a>
                     </p>
