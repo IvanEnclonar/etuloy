79ceed3a43695a2ccf0b0cc35ca7b048ad17fa0c
richjpex <richard_pecson@dlsu.edu.ph>
2023-11-19 12:37:44 +0800

    added new styling

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -86,8 +86,8 @@ function Chat() {
 
     return (
         <section className="bg-center bg-no-repeat bg-blend-multiply bg-background">
-            <div className="ChatApp flex items-center justify-center h-screen bg-black">
-                <div className="relative h-screen-vh h-screen-fill-available w-screen overflow-auto">
+            <div className="ChatApp flex items-center justify-center h-screen bg-black m-8">
+                <div className="relative sm:h-screen w-screen overflow-auto" style={{height: "-webkit-fill-available"}}>
                     <MainContainer style={{ border: "0px solid #343541", }}>
                         <ChatContainer>
                             <MessageList
